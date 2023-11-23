a39bf1be1677c6aa11b9bcede1bff8dc25031a5f
99SupunSulakshana <supunsulak20@gmail.com>
2023-11-23 11:40:44 +0530

    first comment

---
+++ r2-streamer/src/main/java/org/readium/r2/streamer/config/Config.kt
@@ -0,0 +1,38 @@
+package org.readium.r2.streamer.config
+
+import android.content.Context
+
+/**
+ * Created by Nuwan on 3/25/19.
+ */
+class Configurations {
+
+    var needExport: Boolean = false
+    var callback: Callback? = null
+    var imageListener:ImageClickListener?=null
+
+    interface Callback {
+        fun export(input: String): String
+    }
+
+    interface ImageClickListener {
+        fun onImageClick(url: String,context:Context?)
+    }
+
+
+    companion object {
+        private val instance = Configurations()
+
+        fun getInsance(): Configurations {
+            return instance;
+        }
+    }
+
+
+    fun clear() {
+        imageListener=null
+        needExport = false
+        callback = null
+    }
+
+}
